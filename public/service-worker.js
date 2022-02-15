01cf1dad95e27b016c479e5306cf6d807be8fc83
adairconlin <adair.conlin@gmail.com>
2022-02-15 16:22:42 -0500

    Begin README

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,23 +6,24 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
-    "./js/idb.js"
+    "./js/idb.js",
+    "./manifest.json"
 ];
 
-// self.addEventListener("fetch", function(e) {
-//     console.log("fetch request : " + e.request.url)
-//     e.respondWith(
-//         caches.match(e.request).then(function(request) {
-//             if(request) { // if cache is available, respond with cache
-//                 console.log("responding with cache : " + e.request.url)
-//                 return request // if there are no cache, try fetching request
-//             } else {
-//                 console.log("file is not cached fetching : " + e.request.url)
-//                 return fetch(e.request)
-//             }
-//         })
-//     )
-// })
+self.addEventListener("fetch", function(e) {
+    console.log("fetch request : " + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if(request) { // if cache is available, respond with cache
+                console.log("responding with cache : " + e.request.url)
+                return request // if there are no cache, try fetching request
+            } else {
+                console.log("file is not cached fetching : " + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
 
 self.addEventListener("install", function(e) {
     e.waitUntil(
@@ -33,22 +34,22 @@ self.addEventListener("install", function(e) {
     )
 });
 
-// self.addEventListener("activate", function(e) {
-//     e.waitUntil(
-//         caches.keys().then(function(keyList) {
-//             let cacheKeeplist = keyList.filter(function(key) {
-//                 return key.indexOf(APP_PREFIX);
-//             });
-//             cacheKeeplist.push(CACHE_NAME);
+self.addEventListener("activate", function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeeplist = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
 
-//             return Promise.all(
-//                 keyList.map(function(key, i) {
-//                     if(cacheKeeplist.indexOf(key) === -1) {
-//                         console.log("deleting cache : " + keyList[i]);
-//                         return caches.delete(keyList[i]);
-//                     }
-//                 })
-//             )
-//         })
-//     )
-// });
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if(cacheKeeplist.indexOf(key) === -1) {
+                        console.log("deleting cache : " + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+});
